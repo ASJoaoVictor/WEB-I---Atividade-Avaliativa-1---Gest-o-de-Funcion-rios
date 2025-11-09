65cbf52b66856b3630931096aad778c8a46777c5
j.victor.dev <victor.sidiao@academico.ifpb.edu.br>
2025-11-09 21:26:57 +0000

    estatísticas de usuários

--- projeto_inicial/main.py
+++ projeto_inicial/main.py
@@ -7,8 +7,8 @@ senhaPadrao = "123"
 #funcionario: nome, login, senha, tipo(gerente, caixa, serviços_gerais), salario
 
 gerente = Funcionario("julia", "julia", "gerente", 3500.0, 2015, "123")
-caixa = Funcionario("Ana", "ana", "caixa", 2500.0, 2019, "123")
-servico = Funcionario("andressa", "andressa", "servico gerais",1250.0, 2020, "132")
+caixa = Funcionario("ana", "ana", "caixa", 2500.0, 2010, "123")
+servico = Funcionario("andressa", "andressa", "serviços gerais",1250.0, 2020, "132")
 funcionarios = [gerente, caixa, servico]
 current_user = None
 
@@ -45,10 +45,17 @@ def logar(login, senha):
             return True, func
     return False, None
 
+@app.route("/logoff", methods=["GET"])
+def logoff():
+    global current_user
+    current_user = None
+    return redirect(url_for("home"))
+
 @app.route("/menu", methods=["GET"])
 def get_menu():
-    print(current_user.nome)
-    return render_template("menu.html", user= current_user)
+    if current_user:
+        return render_template("menu.html", user= current_user)
+    return "nada"
 
 @app.route("/cadastrar")
 def get_cadastro():
@@ -56,14 +63,14 @@ def get_cadastro():
 
 @app.route("/cadastrar", methods=["POST"])
 def cadastro():
-    nome = request.form.get("nome")
-    login = request.form.get("login")
+    nome = request.form.get("nome").lower()
+    login = request.form.get("login").lower()
     cargo = request.form.get("tipo")
     salario = float(request.form.get("salario"))
     anoAdmissao = int(request.form.get("admissao"))
     senha = request.form.get("senha")
 
-    if(nome == "" or login == "" or salario == "" or senha == ""):
+    if(nome == "" or login == "" or salario == "" or senha == "" or " " in login):
         return render_template("cadastro.html", mensagem = "Campos inválidos")
     else:
         for func in funcionarios:
@@ -71,7 +78,7 @@ def cadastro():
                 return render_template("cadastro.html", mensagem = "Usuário com login já existe")
             
         if(cadastrar(nome, login, senha, cargo, anoAdmissao, salario)):
-            return render_template("login.html")
+            return render_template("login.html", mensagem= "Usuário cadastro com sucesso!")
         else:
             return render_template("cadastro.html", mensagem = "erro ao cadastrar usuário")
 def cadastrar(nome, login, senha, cargo, anoAdmissao, salario):
@@ -81,10 +88,13 @@ def cadastrar(nome, login, senha, cargo, anoAdmissao, salario):
     except:
         return False
 
-@app.route("/listar/Usuarios", methods=["GET", "POST"])
-def listar_usuarios():
-    return render_template("listarUsuario.html", funcionarios= funcionarios)
+@app.route("/listar/usuarios", methods=["GET", "POST"])
+def listar_usuarios(msg=""):
 
+    if not funcionarios:
+        msg = "Nenhum usuário cadastrado!"
+
+    return render_template("listar_usuarios.html", msg= msg, funcionarios= funcionarios)
 
 @app.route("/listar/usuarios/cargo", methods=["POST"])
 def listar_usuarios_cargo():
@@ -96,7 +106,7 @@ def listar_usuarios_cargo():
             if func.cargo == cargo:
                 lista_funcionarios.append(func)
 
-    return render_template("listarUsuario.html", funcionarios= lista_funcionarios)
+    return render_template("listar_usuarios.html", funcionarios= lista_funcionarios)
 
 @app.route("/listar/usuario/nome", methods=["POST"])
 def listar_usuarios_nome():
@@ -105,13 +115,22 @@ def listar_usuarios_nome():
     if nome:
         lista_funcionarios.append(buscar_por_nome(nome))
 
-    return render_template("listarUsuario.html", funcionarios= lista_funcionarios)
-
+    return listar_usuarios()
 def buscar_por_nome(nome):
     for func in funcionarios:
         if nome == func.nome:
             return func
     return None
+
+@app.route("/usuario/remover/<nome>", methods=["GET"])
+def remover_usuario(nome):
+    usuario = buscar_por_nome(nome)
+
+    if(current_user.nome == nome):
+        return listar_usuarios("Não é possível remover usuário atual!")
+    funcionarios.remove(usuario)
+
+    return listar_usuarios("Usuário removido com sucesso!")
 
 @app.route("/usuario/editar/<nome>", methods=["GET"])
 def get_editar(nome):
@@ -127,28 +146,83 @@ def editar_usuario(nome):
     if not usuario:
         return "usuario não encontrado"
     
-    senha = request.form.get("senha")
+    senha_confirmar = request.form.get("senha_confirmar")
     nome = request.form.get("nome")
     login = request.form.get("login")
     anoAdmissao = None
     cargo = request.form.get("tipo")
     salario = request.form.get("salario")
     anoAdmissao = request.form.get("admissao")
+    senha = request.form.get("senha")
 
-    if senha == "123":
+    if senha_confirmar == "123":
         if nome:
             usuario.nome = nome
         if login:
             usuario.login = login
         if anoAdmissao:
-            usuario.anoAdmissao = anoAdmissao
+            usuario.anoAdmissao = int(anoAdmissao)
         if cargo:
             usuario.cargo = cargo
         if salario:
             usuario.salario = float(salario)
+        if senha:
+            usuario.senha = senha
 
-        return redirect(url_for("listar_usuarios"))
+    return render_template("editar_usuario.html", usuario= usuario, mensagem= "Usuário editado com sucesso!")
         
+@app.route("/usuarios/estatisticas", methods=["GET"])
+def usuarios_estatisticas():
+    usuario_mais_antigo = funcionarios[0]
+    usuario_maior_salario = funcionarios[0]
+    media_salarial_gerente = None
+    media_salarial_caixa = None
+    media_salarial_servicos_gerais = None
+    quant_gerente = 0
+    quant_caixa = 0
+    quant_servicos_gerais = 0
+
+    temp_salario_gerente = 0
+    temp_salario_caixa = 0
+    temp_salario_servicos_gerais = 0
+
+    for func in funcionarios:
+        if(func.anoAdmissao < usuario_mais_antigo.anoAdmissao):
+            usuario_mais_antigo = func
+
+        if(func.salario > usuario_maior_salario.salario):
+            usuario_maior_salario = func
+
+        if(func.cargo == "gerente"):
+            print(func.nome)
+            quant_gerente += 1
+            temp_salario_gerente += func.salario
+        elif(func.cargo == "caixa"):
+            print(func.nome)
+            quant_caixa += 1
+            temp_salario_caixa += func.salario
+        elif(func.cargo == "serviços gerais"):
+            print(func.nome)
+            quant_servicos_gerais += 1
+            temp_salario_servicos_gerais += func.salario
+
+    if quant_gerente:
+        media_salarial_gerente = temp_salario_gerente / quant_gerente
+    if quant_caixa:    
+        media_salarial_caixa = temp_salario_caixa / quant_caixa
+    if quant_servicos_gerais:
+        media_salarial_servicos_gerais = temp_salario_servicos_gerais / quant_servicos_gerais
+
+
+    return render_template("usuarios_estatisticas.html", result= (usuario_mais_antigo.nome,
+                                                                  usuario_maior_salario.nome, 
+                                                                  media_salarial_gerente, 
+                                                                  media_salarial_caixa,
+                                                                  media_salarial_servicos_gerais,
+                                                                  quant_gerente,
+                                                                  quant_caixa,
+                                                                  quant_servicos_gerais))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
